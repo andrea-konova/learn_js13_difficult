2e3c5d8ed638965b6d88d928da5d80e61fa8e706
Andrii Konovalenko <an256.ak@gmail.com>
2020-06-24 20:56:57 +0300

    feat: created file structure

--- script/script.js
+++ script/script.js
@@ -1,16 +1,1 @@
 'use strict'
-
-const wrapper = document.querySelector('.wrapper'),
-  color = document.getElementById('color'),
-  change = document.getElementById('change');
-
-const getColor = function() {
-  
-  const randomColor = '#' + Math.floor(Math.random() * 256 * 256 * 256).toString(16);
-
-  wrapper.style.backgroundColor = randomColor;
-  color.textContent = randomColor;
-  change.style.color = randomColor;
-}
-
-change.addEventListener('click', getColor);
