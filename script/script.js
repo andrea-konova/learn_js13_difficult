21448ecf6429f38f7d6af42fd0185beb19c9875c
Andrii Konovalenko <an256.ak@gmail.com>
2020-06-25 20:27:09 +0300

    feat: completed the task

--- script/script.js
+++ script/script.js
@@ -1,33 +1,20 @@
 'use strict'
 
-function DomElement(selector = '#car', height = '150px', width = '300px', bg = 'yellow', fontSize = '20px') {
-  this.selector = selector;
-  this.height = height;
-  this.width = width;
-  this.bg = bg;
-  this.fontSize = fontSize;
-}
-
-DomElement.prototype.createItem = function() {
-    const block = (this.selector.includes('.')) ? document.createElement('div') :
-      (this.selector.includes('#')) ? document.createElement('p') : 
-      console.log('Упс... Что-то пошло не так =(');
-    if (this.selector.includes('.')) {
-      block.className = this.selector.slice(1);
-    } else if (this.selector.includes('#')) {
-      block.id = this.selector.slice(1);
-    }
-    block.style.cssText = `
-      height: ${this.height};
-      width: ${this.width};
-      background-color: ${this.bg};
-      font-size: ${this.fontSize};
-    `;
-    block.innerHTML = 'Привет, мир';
-    document.body.append(block);
-  
+class First {
+  hello() {
+    console.log('Привет я метод родителя!');
+  }
 };
 
-let domElement1 = new DomElement ('.block', '50px', '100px', 'orange', '16px');
+class Second extends First {
+  hello() {
+    super.hello();
+    console.log('А я наследуемый метод!');
+  }
+}
 
-domElement1.createItem();
+const first = new First;
+const second = new Second;
+
+first.hello();
+second.hello();
