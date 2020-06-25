02914b4d4fed59e8f7e8e2f2465d1befc63d579d
Andrii Konovalenko <an256.ak@gmail.com>
2020-06-25 14:49:41 +0300

    fix: fixed main problems

--- script/script.js
+++ script/script.js
@@ -9,26 +9,22 @@ function DomElement(selector, height, width, bg, fontSize) {
 }
 
 DomElement.prototype.createItem = function() {
-  if (this.selector.indexOf('.') != -1) {
-    const div = document.createElement('div');
-    div.className = this.selector;
-    div.style.height = this.height;
-    div.style.width = this.width;
-    div.style.backgroundColor = this.bg;
-    div.style.fontSize = this.fontSize;
-    div.innerHTML = 'Как жизнь?';
-    document.body.append(div);
-  }
-  if (this.selector.indexOf('#') != -1) {
-    let p = document.createElement('p');
-    p.id = this.selector;
-    p.style.height = this.height;
-    p.style.width = this.width;
-    p.style.backgroundColor = this.bg;
-    p.style.fontSize = this.fontSize;
-    p.innerHTML = 'Привет, мир';
-    document.body.append(p);
-  }
+    const block = (this.selector.includes('.')) ? document.createElement('div') :
+      (this.selector.includes('#')) ? document.createElement('p') : 
+      console.log('Что-то пошло не так');
+    if (this.selector.includes('.')) {
+      block.className = this.selector.slice(1);
+    } else if (this.selector.includes('#')) {
+      block.id = this.selector.slice(1);
+    }
+    block.style.cssText = `
+      height: ${this.height};
+      width: ${this.width};
+      background-color: ${this.bg};
+      font-size: ${this.fontSize};
+    `;
+    block.innerHTML = 'Привет, мир';
+    document.body.append(block);
   
 };
 
